a9bf8bcc467d0396901a051abd5bbe0ff5f8a6a4
Oleh Onysko <oleh.onysko3@gmail.com>
2021-07-17 11:04:05 +0200

    add user endpoint

---
+++ accounts/middlewares.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import get_user_model
+from django.utils.timezone import now
+
+
+class SetLastActivityMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if request.user.is_authenticated:
+            # Update last activity after request finished processing
+            get_user_model().objects.filter(pk=request.user.pk).update(last_activity=now())
+
+        response = self.get_response(request)
+
+        return response
